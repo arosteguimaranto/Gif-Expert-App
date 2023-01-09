9614a370a3ad9a1d414ce3dc05e854a095153068
Arostegui Maranto <arosteguimaranto@outlook.com>
2023-01-09 15:25:08 -0600

    GifExpertApp - Component

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,17 @@
+
+
+export const GifExpertApp = () => {
+    return (
+        <>
+            {/* titulo*/}
+            <h1> Gif Expert App </h1>
+
+            {/* input */}
+
+            {/* listado de Gif */}
+
+            {/* Gif item */}
+
+        </>
+    )
+}
