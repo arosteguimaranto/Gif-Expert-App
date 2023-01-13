68ad6c55e86945caeeabaf7245a521ccc953a710
Arostegui Maranto <arosteguimaranto@outlook.com>
2023-01-12 22:18:11 -0600

    Archivos de barril

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -18,7 +18,7 @@ export const GifGrid = ({ category }) => {
       <h3>{category}</h3>
       {
         isLoading
-        && (<h2>Cargando... </h2>)
+        && (<h2> Cargando... </h2>)
        
         
       }
