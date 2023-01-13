f6bee1f8f469f21fb5dd0dee677fa9a278a687d6
Arostegui Maranto <arosteguimaranto@outlook.com>
2023-01-12 22:09:38 -0600

    Mostrar mensaje de carga

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -11,12 +11,18 @@ export const GifGrid = ({ category }) => {
 
   const{images, isLoading} = useFetchGifs(category);
   
-  console.log({ isLoading});
   
   return (
 
     <>
       <h3>{category}</h3>
+      {
+        isLoading
+        && (<h2>Cargando... </h2>)
+       
+        
+      }
+     
 
       <div className="card-grid">
         {
