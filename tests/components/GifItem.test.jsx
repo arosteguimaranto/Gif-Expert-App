a258325b1ab44042b56385281a30202aad69c451
Arostegui Maranto <arosteguimaranto@outlook.com>
2023-02-27 18:58:30 -0600

    Pruebas del componente - GifGridItem

--- tests/components/GifItem.test.jsx
+++ tests/components/GifItem.test.jsx
@@ -12,11 +12,28 @@ describe('Pruebas en GifItem />', () => {
         const { container } = render(<GifItem title ={title} url={url} />);
         expect(container).toMatchSnapshot();
     });
-})
 
-test('Debe mostrar la imagen con el URL y el ALT indicado', () => {
+    test('Debe mostrar la imagen con el URL y el ALT indicado', () => {
 
-    render(<GifItem title={title} url={url} />);
+        render(<GifItem title={title} url={url} />);
+    
+       /*  expect(screen.getByRole('img').src ).toBe(url);
+        expect(screen.getByRole('img').alt ).toBe(title); */
 
-    expect(screen.getByRole('img').src ).toBe(url);
+        const { src, alt } = screen.getByRole('img');
+
+        expect (src).toBe(url);
+        expect(alt).toBe(alt);
+    });
+    
+    test('debe de mostrar el titulo en el componente', () => {
+
+        render( <GifItem title={title} url={url} />);
+        expect(screen.getByText(title).toBeTruthy())
+
+    } );
+
+
+
 });
+
