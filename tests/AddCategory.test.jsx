dbbcaf0a610937883f84bb8ff590887959a191a8
Arostegui Maranto <arosteguimaranto@outlook.com>
2023-02-27 22:33:57 -0600

    Jest Functions

--- tests/AddCategory.test.jsx
+++ tests/AddCategory.test.jsx
@@ -17,10 +17,12 @@ describe('Pruebas en <AddCategory />', () => {
 
     test('debe de llamar onNewCategory si el input tiene un valor', () => {
             const inputValue = 'Saitama';
+            const onNewCategory = jest.fn();
+
 
             //TODO: ???????
 
-            render(<AddCategory onNewCategory={() => {}} /> );
+            render(<AddCategory onNewCategory={onNewCategory} /> );
 
             const input = screen.getByRole('textbox');
             const form = screen.getByRole('form')
@@ -29,6 +31,25 @@ describe('Pruebas en <AddCategory />', () => {
             fireEvent.submit(form);
 
             expect(input.value).toBe('');
+            expect(onNewCategory).toHaveBeenCalled();
+            expect(onNewCategory).toHaveBeenCalledTimes(1);
+            expect(onNewCategory).toHaveBeenCalledWith(inputValue);
+
+    });
+
+    test('no debe llamar el onNewCategory si el input esta vacio ', () => {
+        
+            const onNewCategory = jest.fn();
+
+            render(<AddCategory onNewCategory={onNewCategory} /> );
+
+          
+            const form = screen.getByRole('form')
+            fireEvent.submit(form);
+
+           
+            expect(onNewCategory).toHaveBeenCalledTimes(0);
+            expect(onNewCategory).not.toHaveBeenCalled();    
 
     });
 
